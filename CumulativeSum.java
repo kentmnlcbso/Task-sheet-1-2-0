43e766d27d654bdd3cd83eba6beaaab5926e71b5
Kenneth Emanuel Cabusao <119332036+kentmnlcbso@users.noreply.github.com>
2025-01-05 12:50:41 +0000

    Done adding output

--- CumulativeSum.java
+++ CumulativeSum.java
@@ -1,1 +1,27 @@
+public class CumulativeSum {
 
+    public static int calculateCumulativeSum(int... nums) {
+        int totalSum = 0;
+
+        for (int num : nums) {
+            int cumulativeSum = 0;
+
+            for (int i = 1; i <= num; i++) {
+                cumulativeSum += i;
+            }
+
+            System.out.println("Cumulative sum up to " + num + " is: " + cumulativeSum);
+
+            totalSum += cumulativeSum;
+        }
+
+        return totalSum;
+    }
+
+    public static void main(String[] args) {
+
+        int total = calculateCumulativeSum(4, 5, 10);
+
+        System.out.println("Total sum of all cumulative sums: " + total);
+    }
+}
